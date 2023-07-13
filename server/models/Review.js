4cdbaece161689c34a342cbdb66f7f90fb7679f2
JohnDavidSmith <128341208+JohnDavidSmith@users.noreply.github.com>
2023-07-13 12:04:00 -0400

    Made the Rating.js and Review.js files

--- server/models/Review.js
+++ server/models/Review.js
@@ -1,8 +1,21 @@
-const { Schema } = require("mongoose");
+
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
 
 const reviewSchema = new Schema({
     text: {
         type: String,
         
-    }
+    },
+    ratings: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Rating'
+        }
+      ]   
 })
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
