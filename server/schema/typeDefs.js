18f62b3cfe63c793a1d70daeebb60d7199c94c36
JohnDavidSmith <128341208+JohnDavidSmith@users.noreply.github.com>
2023-07-14 13:18:31 -0400

    New typeDefs.js file

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -3,35 +3,61 @@ const { gql } = require("apollo-server-express");
 const typeDefs = gql`
     type User {
         _id: ID!
-        email: STRING!
-        password: STRING!
+        email: String!
+        password: String!
         gameCart: [Game]
     }
 
     type Game {
         _id: ID!
-        name: STRING!
-        img: STRING!
-        description: STRING!
+        name: String!
+        img: String!
+        description: String!
         reviews: [Review]
     }
     
     type Review {
         _id: ID!
-        text: STRING!
+        text: String!
         ratings: [Rating]
     }
     
     type Rating {
         _id: ID!
-        value: INTEGER!        
+        value: Int!        
     }
 
-    type Query {
-        games: [Game]!
-        game(gameId: ID!): Game
+    type Auth {
+        token: String!
+        user: User
+      }
 
-        
+    input gameInput {
+        name: String!
+        img: String!
+        description: String! 
+      }
+
+    input reviewInput {
+        text: String! 
+      }
+
+    input ratingInput {
+        value: Int!  
+      }
+
+    type Query { 
+        me: User
     }
-    `
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveGame(input: gameInput!): User
+        removeGame(gameId: ID!): User
+        createReview(input: reviewInput!): Game
+        createRating(input: ratingInput!): Review
+      }
+    `;
+
+module.exports = typeDefs;
 
