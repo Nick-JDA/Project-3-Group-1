996ba9684f1eff9ba3176a066ee7807fa2d6cbdd
unknown <loltony79@gmail.com>
2023-07-17 21:28:51 -0400

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -45,9 +45,9 @@ function App() {
           <div>
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login/>}/>
-              <Route path="/signup" element={<Signup/>}/>
-              <Route path="/products/:id" element={Details}/>
+              {/* <Route path="/login" element={<Login/>}/>
+              <Route path="/signup" element={<Signup/>}/> */}
+              {/* <Route path="/products/:id" element={Details}/> */}
 
             </Routes>
           </div>
