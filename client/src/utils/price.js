38b09955d2a19b2ba0d983acd4e38f4785b5b781
unknown <loltony79@gmail.com>
2023-07-17 22:26:17 -0400

    product-list

---
+++ client/src/utils/price.js
@@ -0,0 +1,8 @@
+const getRandomPrice = () => {
+    let price  = Math.floor(Math.random() * 100);
+    price = parseFloat(price);
+    price = price + 0.99;
+    return price; 
+};
+
+module.exports = getRandomPrice;
